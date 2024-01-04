947dd24c5645063c37620e294f5c87b950f7805d
csm <csm009177@gmail.com>
2024-01-04 09:32:48 +0900

    react-7-typescript.ts is about relationship list and component by typescript

---
+++ app.ts
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PokemonList: React.FC = () => {
+
+  const pokemon: string[] = ["피카츄", "라이츄", "파이리", "꼬부기"];
+
+  const updatedPokemon: string[] = pokemon.map((pokemonName) => {
+    if (pokemonName === "파이리") {
+      return "버터풀";
+    }
+    return pokemonName;
+  });
+
+  const pokemonElement = updatedPokemon.map((pokemonName, index) => (
+    <div key={index}>
+      <p>{pokemonName}</p>
+    </div>
+  ));
+
+  return <div>{pokemonElement}</div>;
+};
+
+export default PokemonList;
